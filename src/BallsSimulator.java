b2ee77dae3cd2036426e5d00766865aee0eee39b
baumanar <arnaud.baumann@phelma.grenoble-inp.fr>
2017-10-19 18:04:49 +0200

    ajout de l'affichage graphique

--- src/BallsSimulator.java
+++ src/BallsSimulator.java
@@ -1,22 +1,30 @@
-import gui.Simulable;
-
+import gui.*;
+import java.awt.Color;
 
 public class BallsSimulator extends Balls implements Simulable
 {
 
   public Balls pointsSimulables;
+  public GUISimulator gui;
 
   public BallsSimulator(int nb)
   {
     super(nb);
-  }
+    this.gui =  new GUISimulator (500 , 500 , Color . BLACK ) ;
 
+  }
 
 @Override
 public void next ()
 {
   this.translate(10,10);
   System.out.println(this.toString());
+  this.gui.reset();
+  for(int i=0;i<this.tab.length;i++)
+  {
+    this.gui.addGraphicalElement( new Rectangle(this.tab[i].x, this.tab[i].y,Color.decode("#1f77b4"),Color.decode("#1f77b4"),10) );
+  }
+
 }
 
 @Override
@@ -24,6 +32,12 @@ public void restart ()
 {
   this.reInit();
   System.out.println(this.toString());
+  this.gui.reset();
+  for(int i=0;i<this.tab.length;i++)
+  {
+    this.gui.addGraphicalElement( new Rectangle(this.tab[i].x, this.tab[i].y,Color.decode("#1f77b4"),Color.decode("#1f77b4"),10) );
+  }
+
 }
 
 
--- src/TestBallsSimulator.java
+++ src/TestBallsSimulator.java
@@ -1,14 +1,11 @@
-import gui.GUISimulator ;
+import gui.* ;
 import java.awt.Color ;
 
 public class TestBallsSimulator
 {
   public static void main ( String[] args ) {
-  GUISimulator gui = new GUISimulator (500 , 500 , Color . BLACK ) ;
   BallsSimulator b = new BallsSimulator(4);
-  gui.setSimulable ( b ) ;
-  
-
+  b.gui.setSimulable ( b ) ;
 
 }
 
