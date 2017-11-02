cc57dcfd4c3769c7d25c071d785021c10e13dc09
Lea Elhaik <lea.el-haik@ensimag.grenoble-inp.fr>
2017-11-02 18:28:50 +0100

    revert

---
+++ src/TestBoidsSimulator.java
@@ -0,0 +1,14 @@
+import gui.* ;
+import simulators.BoidsSimulator;
+
+import java.awt.Color ;
+//PERMET D'AFFICHER LES BOIDS SUR LA SIMULATION GRAPHIQUE
+public class TestBoidsSimulator
+{
+  public static void main ( String[] args ) {
+  BoidsSimulator b = new BoidsSimulator(1,1);
+  b.gui.setSimulable ( b ) ;
+
+}
+
+}
---
+++ src/boids/Predator.java
@@ -0,0 +1,68 @@
+package boids;
+
+public class Predator extends BoidNew {
+
+
+	public Predator(int x, int y, double vx, double vy, double ax, double ay) {
+		super(x, y, vx, vy, ax, ay);
+	}
+
+
+	public void ruleDistance(BoidsNew BoidsHerds) //Garder une distance minimale séparant les Boid
+	  {
+	    for (int i =0; i<BoidsHerds.PredatorsTab.length; i ++)
+	    {
+	      int cX = 0;
+	      int cY = 0;
+	      for( int j = 0; (j < BoidsHerds.PredatorsTab.length) ; j++)
+	      {
+	        if (j !=i)
+	        {
+	          if (Math.abs(BoidsHerds.PredatorsTab[i].x - BoidsHerds.PredatorsTab[j].x) < 50 ) //Si la distance est trop petite
+	          {
+	            cX += -(BoidsHerds.PredatorsTab[i].x - BoidsHerds.PredatorsTab[j].x);//On décrémente cX de la distance trop courte
+	          }
+	          if (Math.abs(BoidsHerds.PredatorsTab[i].y - BoidsHerds.PredatorsTab[j].y) < 50 )
+	          {
+	            cY += -(BoidsHerds.PredatorsTab[i].y - BoidsHerds.PredatorsTab[j].y);//On décrémente cX de la distance trop courte
+	          }
+	        }
+	      }
+	      BoidsHerds.PredatorsTab[i].addAccelerationX(cX); //On retranche cette distance à l'accelerationX
+	      BoidsHerds.PredatorsTab[i].addAccelerationX(cY); //On retranche cette distance à l'accelerationX
+
+	    }
+	  }
+
+	@Override
+	public void move(BoidsNew boidsHerds) {
+		resetAcceleration();
+		ruleDistance(boidsHerds);
+		this.boundPosition(1200,800,0,0);
+		update();
+	}
+
+	@Override
+	public void flee(BoidsNew BbidsHerds) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void hunt(BoidsNew boidsHerds) {
+		for( int j = 0; (j < boidsHerds.PreysTab.length); j++)
+		{
+				double sumOrientation=0;
+				int counter=1;
+			 if (this.distanceFrom(boidsHerds.PreysTab[j]) < 100000)
+			 {
+				sumOrientation += boidsHerds.PreysTab[j].getOrientation();
+				counter++;
+			 }
+			 this.setOrientation(sumOrientation/counter);
+		}
+
+	}
+
+
+}
